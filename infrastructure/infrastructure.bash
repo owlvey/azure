77648e70047e4ee10e2b0c73ded875bd3a8c9b8a
gregory <gregory@gregorys-MacBook-Pro.local>
2020-01-12 15:09:55 -0500

    add

--- infrastructure/infrastructure.bash
+++ infrastructure/infrastructure.bash
@@ -1,2 +1,9 @@
 #!/bin/bash
-echo 'test'
+
+resource_group="owlvey-prod-rg"
+
+az configure --defaults location=eastus
+
+az group create -n $resource_group
+
+az group deployment validate  --resource-group $resource_group --template-file "template.json"  --parameters @parameters.json
